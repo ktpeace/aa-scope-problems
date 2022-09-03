9fa46b646aa565ecb9a58642bc47175f3dccbe2b
ktpeace <katpeace@protonmail.com>
2022-09-03 17:00:46 -0400

    initial commit

---
+++ 03-arrow-rest-sum.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+Write a function named `arrowRestSum` that accepts all incoming parameters
+and sums them. Assign the below function to a variable using the const keyword.
+
+**Hint**: Use rest parameter syntax!
+
+Examples:
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+arrowRestSum(0); // => 0
+***********************************************************************/
+
+const arrowRestSum = (...nums) => {
+  return nums.reduce((acc,next) => acc + next, 0);
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = arrowRestSum;
+} catch (e) {
+  return undefined;
+}
